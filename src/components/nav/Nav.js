656e531cc09ebc5f05d82eb3ffde42c40934155c
Daniel <danielbeloded@gmail.com>
2024-02-06 15:30:08 +0200

    fix

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -32,43 +32,45 @@ const Nav = () => {
 
   return (
     <>
-      <div className="nav">
-        <div className="nav__wrapper">
-          <div className="nav__wrapper-logo">
-            <img src={logo} alt="logo" />
+      <nav>
+        <div className="nav">
+          <div className="nav__wrapper">
+            <div className="nav__wrapper-logo">
+              <img src={logo} alt="logo" />
+            </div>
+
+            <div className="nav__wrapper-list">
+              <ul className="nav__list">
+                <li className="nav__menu">
+                  <img onClick={handleClick} src={menu} alt="menu" />
+                </li>
+                <li className="nav__list-item">Home</li>
+                <li className="nav__list-item">New</li>
+                <li className="nav__list-item">Popular</li>
+                <li className="nav__list-item">Trending</li>
+                <li className="nav__list-item">Categories</li>
+              </ul>
+            </div>
           </div>
 
-          <div className="nav__wrapper-list">
-            <ul className="nav__list">
-              <div className="nav__menu">
-                <img onClick={handleClick} src={menu} alt="menu" />
-              </div>
-              <li className="nav__list-item">Home</li>
-              <li className="nav__list-item">New</li>
-              <li className="nav__list-item">Popular</li>
-              <li className="nav__list-item">Trending</li>
-              <li className="nav__list-item">Categories</li>
-            </ul>
-          </div>
-        </div>
-
-        {/* <div
+          {/* <div
         className={show ? "nav__burger nav__burger-active" : "nav__burger"}
       ></div> */}
-      </div>
-
-      <div className="nav__burger" style={show ? { opacity: 1, top: 0 } : {}}>
-        <div className="nav__burger-btn" onClick={handleClose}>
-          <img src={menuClose} alt="" />
         </div>
-        <ul className="nav__burger-list">
-          <li className="nav__burger-item">Home</li>
-          <li className="nav__burger-item">New</li>
-          <li className="nav__burger-item">Popular</li>
-          <li className="nav__burger-item">Trending</li>
-          <li className="nav__burger-item">Categories</li>
-        </ul>
-      </div>
+
+        <div className="nav__burger" style={show ? { opacity: 1, top: 0 } : {}}>
+          <div className="nav__burger-btn" onClick={handleClose}>
+            <img src={menuClose} alt="" />
+          </div>
+          <ul className="nav__burger-list">
+            <li className="nav__burger-item">Home</li>
+            <li className="nav__burger-item">New</li>
+            <li className="nav__burger-item">Popular</li>
+            <li className="nav__burger-item">Trending</li>
+            <li className="nav__burger-item">Categories</li>
+          </ul>
+        </div>
+      </nav>
     </>
   );
 };
--- src/components/news/News.js
+++ src/components/news/News.js
@@ -5,69 +5,73 @@ import mobileHero from "../../assets/image-web-3-mobile.jpg";
 
 const News = () => {
   return (
-    <div className="news">
-      <div className="news__wrapper">
-        <div className="news__hero">
-          <div className="news__hero-img">
-            <img src={hero} alt="hero" className="news__hero-desk" />
+    <main>
+      <div className="news">
+        <div className="news__wrapper">
+          <div className="news__hero">
+            <div className="news__hero-img">
+              <img src={hero} alt="hero" className="news__hero-desk" />
 
-            <img
-              src={mobileHero}
-              alt="mobileHero"
-              className="news__mobile-hero"
-            />
+              <img
+                src={mobileHero}
+                alt="mobileHero"
+                className="news__mobile-hero"
+              />
+            </div>
+            <div className="news__hero-info">
+              <div className="news__info-title">
+                The Bright Future of Web 3.0?
+              </div>
+              <div className="news__info-aside">
+                <div className="news__info-subtitle">
+                  We dive into the next evolution of the web that claims to put
+                  the power of the platforms back into the hands of the people.
+                  But is it really fulfilling its promise?
+                </div>
+                <div className="news__info-button">
+                  <button className="btn">Read more</button>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="news__hero-info">
-            <div className="news__info-title">
-              The Bright Future of Web 3.0?
-            </div>
-            <div className="news__info-aside">
-              <div className="news__info-subtitle">
-                We dive into the next evolution of the web that claims to put
-                the power of the platforms back into the hands of the people.
-                But is it really fulfilling its promise?
-              </div>
-              <div className="news__info-button">
-                <button className="btn">Read more</button>
-              </div>
-            </div>
-          </div>
-        </div>
 
-        <div className="news__aside">
-          <div className="news__aside-info">
-            <div className="news__aside-title">New</div>
+          <div className="news__aside">
+            <div className="news__aside-info">
+              <div className="news__aside-title">New</div>
 
-            <div className="news__aside-text">
-              <div className="news__aside-promo">Hydrogen VS Electric</div>
-              <div className="news__aside-subtext">
-                Will hydrogen-fueled cars ever catch up to EVs?
+              <div className="news__aside-text">
+                <div className="news__aside-promo">Hydrogen VS Electric</div>
+                <div className="news__aside-subtext">
+                  Will hydrogen-fueled cars ever catch up to EVs?
+                </div>
+                <div className="news__aside-line"> </div>
               </div>
-              <div className="news__aside-line"> </div>
-            </div>
 
-            <div className="news__aside-text">
-              <div className="news__aside-promo">
-                The Downsides of AI Artistry
+              <div className="news__aside-text">
+                <div className="news__aside-promo">
+                  The Downsides of AI Artistry
+                </div>
+                <div className="news__aside-subtext" style={{ width: "290px" }}>
+                  What are the possible adverse effects of on-demand AI image
+                  generation?
+                </div>
+                <div className="news__aside-line"> </div>
               </div>
-              <div className="news__aside-subtext" style={{ width: "290px" }}>
-                What are the possible adverse effects of on-demand AI image
-                generation?
-              </div>
-              <div className="news__aside-line"> </div>
-            </div>
 
-            <div className="news__aside-text">
-              <div className="news__aside-promo">Is VC Funding Drying Up?</div>
-              <div className="news__aside-subtext" style={{ width: "290px" }}>
-                Private funding by VC firms is down 50% YOY. We take a look at
-                what that means.
+              <div className="news__aside-text">
+                <div className="news__aside-promo">
+                  Is VC Funding Drying Up?
+                </div>
+                <div className="news__aside-subtext" style={{ width: "290px" }}>
+                  Private funding by VC firms is down 50% YOY. We take a look at
+                  what that means.
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </main>
   );
 };
 
